130b501bb5d85b1782fb4ff97cf786dc46c2f76b
HAMID <owoyemiameed@gmaiil.com>
2024-07-07 09:03:54 +0100

    unused import fixed

--- src/components/Home/PopularProducts.tsx
+++ src/components/Home/PopularProducts.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import useProximityHover from "../../utils/useProximityHover";
 import Button from "../UI/Button";
@@ -16,7 +16,7 @@ const PopularProducts: React.FC = () => {
     innerElSelector: "button",
   });
 
-  const [special, setSpecial] = useState<SpecialItemProps[]>([
+  const [special] = useState<SpecialItemProps[]>([
     {
       name: "Cheese Burger",
       img: "https://images.unsplash.com/photo-1508736793122-f516e3ba5569?q=80&w=1286&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
